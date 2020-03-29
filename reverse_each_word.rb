66bea5f2030e477b46268a3bdfa3960e183246a2
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-29 21:50:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
 def reverse_each_word(string)
   original_string_array = string.split
-  reverse_array
+  reverse_array = []
   
-  string
+  original_string_array.each do |string|
+    reverse_array << string.reverse
+  end
+  reverse_array.join(" ")
+end
